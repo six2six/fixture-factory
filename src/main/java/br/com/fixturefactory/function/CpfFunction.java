fcf5364582b63cef3ad0e070a3d1a59326338a51
Anderson Parra <ander.pp@gmail.com>
2011-10-26 22:43:26 -0200

    conflito hell

--- src/main/java/br/com/fixturefactory/function/CpfFunction.java
+++ src/main/java/br/com/fixturefactory/function/CpfFunction.java
@@ -17,22 +17,22 @@ public class CpfFunction implements Function {
 	public <T> T generateValue() {
 		RandomFunction random = new RandomFunction(Integer.class, new Range(1, 9));
 
-		Integer a = random.generateValue();
-		Integer b = random.generateValue();
-		Integer c = random.generateValue();
-		Integer d = random.generateValue();
-		Integer e = random.generateValue();
-		Integer f = random.generateValue();
-		Integer g = random.generateValue();
-		Integer h = random.generateValue();
-		Integer i = random.generateValue();
+		int a = random.generateValue();
+		int b = random.generateValue();
+		int c = random.generateValue();
+		int d = random.generateValue();
+		int e = random.generateValue();
+		int f = random.generateValue();
+		int g = random.generateValue();
+		int h = random.generateValue();
+		int i = random.generateValue();
 
-		Integer j = (a * 10 + b * 9 + c * 8 + d * 7 + e * 6 + f * 5 + g * 4 + h * 3 + i * 2);
+		int j = (a * 10 + b * 9 + c * 8 + d * 7 + e * 6 + f * 5 + g * 4 + h * 3 + i * 2);
 		j = j % 11;
 
 		j = j <= 1? 0 : 11 - j;
 
-		Integer m = (a * 11 + b * 10 + c * 9 + d * 8 + e * 7 + f * 6 + g * 5 + h * 4 + i * 3 + j * 2);
+		int m = (a * 11 + b * 10 + c * 9 + d * 8 + e * 7 + f * 6 + g * 5 + h * 4 + i * 3 + j * 2);
 		m = m % 11;
 
 		m = m <= 1? 0 : 11 - m;
