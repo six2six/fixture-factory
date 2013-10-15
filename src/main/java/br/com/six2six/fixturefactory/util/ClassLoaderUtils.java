3c76e5354991359c6dbe998a78a0281a049afc88
Leonardo Comelli <leonardo.comelli@gmail.com>
2013-10-15 17:32:02 -0300

    Revert "fix indentation"
    This reverts commit e391b94fd89ae1d069a9c5fd911746001f54d631.

--- src/main/java/br/com/six2six/fixturefactory/util/ClassLoaderUtils.java
+++ src/main/java/br/com/six2six/fixturefactory/util/ClassLoaderUtils.java
@@ -13,72 +13,72 @@ import java.util.jar.JarInputStream;
 
 public class ClassLoaderUtils {
 
-	public static Set<Class<?>> getClassesForPackage(String packageName) {
-		Set<Class<?>> classes = new HashSet<Class<?>>();
+    public static Set<Class<?>> getClassesForPackage(String packageName) {
+        Set<Class<?>> classes = new HashSet<Class<?>>();
+        
+        JarInputStream jarInputStream = null;
+        try {
+            ClassLoader classLoader = Thread.currentThread().getContextClassLoader();
 
-		JarInputStream jarInputStream = null;
-		try {
-			ClassLoader classLoader = Thread.currentThread().getContextClassLoader();
-
-			Enumeration<URL> resources = classLoader.getResources(packageName.replace('.', '/'));
-			while (resources.hasMoreElements()) {
-				URL res = resources.nextElement();
-
-				if (res.getProtocol().matches("(jar|zip)")) {
-					res = new URL((res.getProtocol().equals("zip") || !res.getFile().contains("file:")  ? 
-							"file:".concat(res.getFile().startsWith("/") ? "": "/") : "").concat(res.getFile().split("!")[0]));
-				}
-				if (res.getFile().contains(".jar")) {
-					jarInputStream = new JarInputStream(res.openStream());
-					JarEntry entry = jarInputStream.getNextJarEntry();
+            Enumeration<URL> resources = classLoader.getResources(packageName.replace('.', '/'));
+            while (resources.hasMoreElements()) {
+                URL res = resources.nextElement();
+                
+                if (res.getProtocol().matches("(jar|zip)")) {
+                    res = new URL((res.getProtocol().equals("zip") || !res.getFile().contains("file:")  ? 
+                            "file:".concat(res.getFile().startsWith("/") ? "": "/") : "").concat(res.getFile().split("!")[0]));
+                }
+                if (res.getFile().contains(".jar")) {
+                    jarInputStream = new JarInputStream(res.openStream());
+                    JarEntry entry = jarInputStream.getNextJarEntry();
 
 
-					while (entry != null) {
-						if (entry.getName().startsWith(packageName.replace('.', '/')) && entry.getName().endsWith(".class") && !entry.getName().contains("$")) {
-							classes.add(Class.forName(entry.getName().replace(".class","").replace("/",".")));
-						}
+                    while (entry != null) {
+                        if (entry.getName().startsWith(packageName.replace('.', '/')) && entry.getName().endsWith(".class") && !entry.getName().contains("$")) {
+                            classes.add(Class.forName(entry.getName().replace(".class","").replace("/",".")));
+                        }
 
-						entry = jarInputStream.getNextJarEntry();
-					}
-				} else {
-					classes.addAll(scanClassesFromDirectory(new File(URLDecoder.decode(res.getPath(), "UTF-8")), packageName));
-				}   
-			}
-		} catch (Exception x) {
-			throw new IllegalArgumentException("invalid package");
-		} finally {
-			if(jarInputStream != null){
-				try {
+                        entry = jarInputStream.getNextJarEntry();
+                    }
+                } else {
+                    classes.addAll(scanClassesFromDirectory(new File(URLDecoder.decode(res.getPath(), "UTF-8")), packageName));
+                }   
+            }
+        } catch (Exception x) {
+            throw new IllegalArgumentException("invalid package");
+        } finally {
+        	if(jarInputStream != null){
+        		try {
 					jarInputStream.close();
 				} catch (IOException e) {
 					throw new IllegalStateException(e.getMessage());
 				}
-			}
-		}
+        	}
+        }
 
-		return classes;
-	}
+        return classes;
+    }
 
-	private static Collection<Class<?>> scanClassesFromDirectory(File directory, String packageName) {
-		Set<Class<?>> classes = new HashSet<Class<?>>();
+    private static Collection<Class<?>> scanClassesFromDirectory(File directory, String packageName) {
+        Set<Class<?>> classes = new HashSet<Class<?>>();
 
-		if (directory.exists() && directory.isDirectory()) {
-			String[] files = directory.list();
-			for (String fileItem : files) {
-				if (fileItem.endsWith(".class")) {
-					try {
-						classes.add(Class.forName(packageName.concat(".").concat(fileItem.replace(".class",""))));
-					} catch (Exception e) {
-						throw new IllegalArgumentException("Invalid package " + packageName);
-					}
-				} else if (!fileItem.matches(".+\\..+")) {
-					File dir = new File(directory,fileItem);
-					if (dir.isDirectory()) {
-						classes.addAll(scanClassesFromDirectory(dir, packageName.concat(".").concat(fileItem)));
-					}
-				}
-			}
-		}
-		return classes;
-	}
+        if (directory.exists() && directory.isDirectory()) {
+            String[] files = directory.list();
+            for (String fileItem : files) {
+                if (fileItem.endsWith(".class")) {
+                    try {
+                        classes.add(Class.forName(packageName.concat(".").concat(fileItem.replace(".class",""))));
+                    } catch (Exception e) {
+                        throw new IllegalArgumentException("Invalid package " + packageName);
+                    }
+                } else if (!fileItem.matches(".+\\..+")) {
+                    File dir = new File(directory,fileItem);
+                    if (dir.isDirectory()) {
+                    	classes.addAll(scanClassesFromDirectory(dir, packageName.concat(".").concat(fileItem)));
+                    }
+                }
+            }
+        }
+        return classes;
+    }
 }
