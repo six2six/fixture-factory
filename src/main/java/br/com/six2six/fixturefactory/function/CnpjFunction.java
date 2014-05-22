b983afb555330a7dd65d95bdb4ced1b82b13c28e
Felipe Adorno <felipeadsc@gmail.com>
2014-05-22 02:00:49 -0300

    Fix function of generate CNPJ
    Fix function of generate CNPJ

--- src/main/java/br/com/six2six/fixturefactory/function/CnpjFunction.java
+++ src/main/java/br/com/six2six/fixturefactory/function/CnpjFunction.java
@@ -31,11 +31,10 @@ public class CnpjFunction implements AtomicFunction {
 		Integer m = 1;
 		
 		int n = m*2+l*3+j*4+i*5+h*6+g*7+f*8+e*9+d*2+c*3+b*4+a*5;
-		n = 11 - (n % 11);
-		if (n>=10) n = 0;
+		n = n % 11 < 2 ? 0 : 11 - (n % 11);
+
 		int o = n*2+m*3+l*4+j*5+i*6+h*7+g*8+f*9+e*2+d*3+c*4+b*5+a*6;
-		o = 11 - (o % 11);
-		if (o>=10) o = 0;
+		o = o % 11 < 2 ? 0 : 11 - (o % 11);
 		
 		return (T) String.format(formatted? "%d%d.%d%d%d.%d%d%d/%d%d%d%d-%d%d" : "%d%d%d%d%d%d%d%d%d%d%d%d%d%d", a, b, c, d, e, f, g, h, i, j, l, m, n, o);
 	
