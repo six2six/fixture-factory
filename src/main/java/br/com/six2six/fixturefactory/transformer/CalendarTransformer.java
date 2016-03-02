8af46e0c9d2c0791a6dceb97bb95ca887b035202
mundodojava <ismaelcosta1@live.com>
2016-03-02 09:53:46 -0300

    identation

--- src/main/java/br/com/six2six/fixturefactory/transformer/CalendarTransformer.java
+++ src/main/java/br/com/six2six/fixturefactory/transformer/CalendarTransformer.java
@@ -48,10 +48,10 @@ public class CalendarTransformer implements Transformer {
 	public boolean accepts(Object value, Class<?> type) {
     	boolean instanceOfCalendar = value instanceof Calendar;
     	
-		try {
-			return JavaVersion.current().gte(JavaVersion.JAVA_8) ? instanceOfCalendar && !ClassUtils.getClass("java.time.temporal.Temporal").isAssignableFrom(type) : instanceOfCalendar;
-		} catch (ClassNotFoundException e) {
-			throw new IllegalArgumentException(e.getMessage(),e);
-		}
+	try {
+		return JavaVersion.current().gte(JavaVersion.JAVA_8) ? instanceOfCalendar && !ClassUtils.getClass("java.time.temporal.Temporal").isAssignableFrom(type) : instanceOfCalendar;
+	} catch (ClassNotFoundException e) {
+		throw new IllegalArgumentException(e.getMessage(),e);
+	}
     }
 }
